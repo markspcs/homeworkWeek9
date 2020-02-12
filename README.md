65e6ba15fc88d428e9a6be6baefc303796a05aef
markspcs <you@example.com>
2020-02-11 21:18:14 -0600

    finished

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# homeworkWeek9
+# Portfolio PDF
+https://markspcs.github.io/homeworkWeek9/
+
+## Description
+
+This Node.js application uses the username provided during execution to query github.com, and gets varouis information. That is saved, and formatted into a PDF
+
+![screen shot](./example.png)
+## Usage
+execute by typing:
+    node index.js
+
+    at the prompt enter the github.com username.
+    then at the next prompt enter your favorite color, which will be used to color the background
+    containers showing the users information. If you use WHITE, you may not be able to see the user information.
+## Installation
+
+Requires Inquirer, Axios, and html-pdf
+
+## Issues
+If you execute this script consecutively  or other scripts that queries the github API, 
+YOU WILL NOT get a file created because github rate limits the amount of traffic.
+
+## Credits
+
+This program includes bootstrap from http://getbootstrap.com 
+Also fontawsome from https://www.fontawesome.com
+
+
--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 const pdf = require('html-pdf');
-const fs = require('fs');
-const html = fs.readFileSync('./test.html', 'utf8');
-// const option = { format: 'A4', orientation: 'portrait', viewportSize: {
-//   width: 1200,
-//   height: 600
-// },
-// };
+//const fs = require('fs');
+//Turn console logs on or off
+const debug = 0;
 
-const option = { height: "10in", 
-  width: "6in",
-  margin: '1cm',
-  viewportSize: { width: 800, height: 400 }
-};
 
-pdf.create(html, option).toFile('./test.pdf', function(err, res){
-  if (err) return console.log(err);
-  console.log(res);
-});
+
 var githubInfo = {};
 var gitUsername;
-var numRepos, numFollowing, numFollowers, githubName, githubAvatar, githubLocation, githubBio, githubBlog, githubCompany;
 
 //Added this part, because I need a proxy in one of my development enviroments
 //global var to be used across any function
@@ -44,18 +31,14 @@ else {
 
 
 asyncFunc();
-console.log("here")
 ////////////////////////////////////////////////
 //ALL ASYNC CONTENT HERE
 async function asyncFunc() {
   const response = await promptUser();
-  console.log(response);
+  debug && console.log(`prompt response object : ${response}`);
   gitUsername = response.username;
   let resUserInfo = await axiosCall(`https://api.github.com/users/${response.username}`);
-  let resRepos = await axiosCall(`https://api.github.com/users/${response.username}/repos?per_page=100`);
-  let resFollowers = await axiosCall(`https://api.github.com/users/${response.username}/followers`);
-  let resFollowing = await axiosCall(`https://api.github.com/users/${response.username}/following`);
-  console.log(resUserInfo.data);
+  debug && console.log(resUserInfo.data);
   githubInfo = {
     numRepos: resUserInfo.data.public_repos,
     numStars: resUserInfo.data.public_gists,
@@ -66,29 +49,25 @@ async function asyncFunc() {
     githubLocation: resUserInfo.data.location,
     githubBio: resUserInfo.data.bio,
     githubBlog: resUserInfo.data.blog,
-    githubCompany: resUserInfo.data.company
+    githubCompany: resUserInfo.data.company,
+    githubWeb: `https://github.com/${gitUsername}`,
+    favColor: response.color
   }
-  console.log(githubInfo);
+  const option = {
+    height: "10in",
+    width: "10in",
+    margin: '1cm',
+    viewportSize: { width: 400, height: 400 }
+  };
+  var page = createHTML();
+  pdf.create(page, option).toFile("./portfolio.pdf", function (err, res) {
+    if (err) return console.log(err);
+    debug && console.log(`response ${res}` );
+  });
+  debug && console.log(`githubInfo ${githubInfo}`);
 
 
-  //console.log(`user ${gitUsername} git ${numRepos} follower ${numFollowers} following ${numFollowing} ${githubAvatar}`);
-  console.log(createHTML());
-
-//   var pdfConvert = convertFactory({
-//     converterPath: convertFactory.converters.PDF
-//   });
-
-//   pdfConvert({ html: createHTML() }, function (err, result) {
-//     if (err) {
-//       return console.error(err)
-//     }
-//     console.log(result.numberOfPages);
-//     console.log(result.logs);
-//     result.stream.pipe(fs.createWriteStream('./test.pdf'));
-//     pdfConvert.kill(); // not sure I need this yet
-
-//   });
-// }
+  debug && console.log(createHTML());
 
 }
 
@@ -118,147 +97,141 @@ function createHTML() {
   <html lang="en-us">
   
   <head>
-      <meta charset="UTF-8">
-      <title>profolio</title>
-      <!-- <link rel="stylesheet" type="text/css" href="./assets/css/reset.css"> -->
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <!-- <link rel="stylesheet" href="./style.css"> -->
-      <style>
+    <meta charset="UTF-8">
+    <title>profolio</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+    <style>
       html {
-        height: 100%;
-    }
-    body {
-        background-color: #879cdf;
-        background: linear-gradient(
-            #879cdf ,
-            #879cdf 33%,
-            #dfedee 33%,
-            #dfedee 66%,
-            #879cdf 66%,
-            #879cdf 0px
-           
-            );
-        min-height: 100%;
-        height: 100%;
-        width: 100%;
-        color: white;
-        font-family: Georgia, serif;
-        font-weight: 800;
-        font-size: 22px;
-    }
-    .topCard{
-        overflow: hidden;
-        background-color: #fe8373;
-        padding-top: 100px;
-    }
-    /* #crop{
-        width: 200px;
-        height: 200px;
-        border-radius: 50%;
-        overflow: hidden;
-        position: relative;
-    } */
-    /* #avatar{
-        display: inline;
-        margin: 0 auto;
-        height: 100%;
-        width: auto;
-    } */
-    
-    img.avatar{
-        position: absolute;
-        left: 50%;
-        top: 100px;
-        transform: translate(-50%, -50%);
-        display: inline;
-        background-color: yellow;
-        width: 200px;
-        height: 200px;
-    
-        border-radius: 50%;
-        border: 3px solid yellow;
-    }
-    a {
-        color: white;
-    }
-    .black{
-        color: black;
-        text-align: center;
-    }
-    .bubble {
-        margin: 100px auto;
-        margin-bottom: 40px;
-        overflow: auto;
-        background-color: transparent;
-    }
-    .bodyCont{
-        margin: 0 auto;
-        align-content: center;
-    }
-    .dynCont{
-        width: 40%;
-        background-color: #fe8373;
-        display: inline-block;
-        margin: 10px auto;
-        overflow: auto;
-    }
-    .right{
-        float:right;
-    }
-      </style>
+      height: 100%;
+  }
+  body {
+      background-color: #879cdf;
+      background: linear-gradient(
+          #879cdf ,
+          #879cdf 33%,
+          #dfedee 33%,
+          #dfedee 66%,
+          #879cdf 66%,
+          #879cdf 0px
+         
+          );
+      min-height: 100%;
+      height: 100%;
+      width: 100%;
+      color: white;
+      font-family: Georgia, serif;
+      font-weight: 800;
+      font-size: 22px;
+  }
+  .topCard{
+      overflow: hidden;
+      background-color: ${githubInfo.favColor};
+      padding-top: 100px;
+  }
+  .bold{
+    font-weight: bold;
+  }
+  img.avatar{
+      position: absolute;
+      left: 390px;
+      top: 1px;
+      transform: translate(-50%, -50%);
+      display: inline;
+      background-color: yellow;
+      width: 200px;
+      height: 200px;
+  
+      border-radius: 50%;
+      border: 3px solid yellow;
+  }
+  a {
+      color: white;
+  }
+  .black{
+      color: black;
+      text-align: center;
+  }
+  .bubble {
+      margin: 100px auto;
+      margin-bottom: 40px;
+      overflow: auto;
+      background-color: transparent;
+  }
+  .bodyCont{
+      margin: 0 auto;
+      align-content: center;
+  }
+  .dynCont{
+      width: 40%;
+      background-color: ${githubInfo.favColor};
+      display: inline-block;
+      margin: 10px auto;
+      overflow: auto;
+  }
+  .right{
+      float:right;
+  }
+  .clickInfo {
+    font-size: 12px;
+    margin-left: 8px;
+  }
+    </style>
   </head>
+  
   <body>
-
-  <div class="card w-75 text-center bubble">
-
-    <div class="card-body topCard">
-
-
-
-      <h5 class="card-title">Hi!</h5>
-      <p class="card-text">My name is Greg.</p>
-      <p> Currently @ @influxdata </p>
-      <a href="https://www.google.com/maps/place/St George, UT"><i class="fas fa-location-arrow"></i>St George, UT</a>
-      <i class="fab fa-github"></i>
-      <i class="fas fa-rss"></i>
-    </div>
-  </div>
-
-  <img class="avatar" src="https://avatars0.githubusercontent.com/u/2653109?v=4" />
-
-  <div class="bodyCont w-50">
-    <h3 class="black">I'm an aspiring go pro. I write codes, shoot guns, climb rocks, and eat meats.</h3>
-    <div class="card text-center dynCont">
-      <div class="card-body">
-        <h5 class="card-title">Public Repositories</h5>
-        <p class="card-text">28.</p>
+  
+    <div class="card w-75 text-center bubble">
+  
+      <div class="card-body topCard">
+  
+  
+  
+        <h3 class="card-title">Hi!</h3>
+        <p class="card-text">My name is ${githubInfo.githubName}!</p>
+        <p><h5> Currently @ ${githubInfo.githubCompany} </h5></p>
+        <a class="clickInfo" href="https://www.google.com/maps/place/${githubInfo.githubLocation}"><i class="fas fa-location-arrow"></i> ${githubInfo.githubLocation}   </a>
+        <a class="clickInfo" href="${githubInfo.githubWeb}><i class="fab fa-github"></i> GitHub   </a>
+        <a class="clickInfo" href="${githubInfo.githubBlog}"><i class="fas fa-rss"></i> Blog</a>
       </div>
     </div>
-    <div class="card text-center dynCont right">
-      <div class="card-body">
-        <h5 class="card-title">Followers</h5>
-        <p class="card-text">25</p>
+  
+    <img class="avatar" src="${githubInfo.githubAvatar}" />
+  
+    <div class="bodyCont w-50">
+      <h3 class="black">${githubInfo.githubBio}</h3>
+      <div class="card text-center dynCont">
+        <div class="card-body">
+          <h5 class="card-title">Public Repositories</h5>
+          <p class="card-text">${githubInfo.numRepos}</p>
+        </div>
+      </div>
+      <div class="card text-center dynCont right">
+        <div class="card-body">
+          <h5 class="card-title"><br>Followers</h5>
+          <p class="card-text">${githubInfo.numFollowers}</p>
+        </div>
+      </div>
+  
+      </p>
+      <div class="card text-center dynCont">
+        <div class="card-body">
+          <h5 class="card-title">GitHub Stars</h5>
+          <p class="card-text">${githubInfo.numStars}</p>
+        </div>
+      </div>
+      <div class="card text-center dynCont right">
+        <div class="card-body">
+          <h5 class="card-title">Following</h5>
+          <p class="card-text">${githubInfo.numFollowing}</p>
+        </div>
       </div>
     </div>
-
-    </p>
-    <div class="card text-center dynCont">
-      <div class="card-body">
-        <h5 class="card-title">GitHub Stars</h5>
-        <p class="card-text">5</p>
-      </div>
-    </div>
-    <div class="card text-center dynCont right">
-      <div class="card-body">
-        <h5 class="card-title">Following</h5>
-        <p class="card-text">1</p>
-      </div>
-    </div>
-  </div>
-</body>
-
-</html>
+  </body>
+  
+  </html>
 
 `;
 }
@@ -285,7 +258,7 @@ function createHTML() {
 //                 <p class="card-text">${githubInfo.numFollowers}</p>
 //               </div>
 //           </div>
-  
+
 //       </p>
 //           <div class="card w-25 text-center dynCont">
 //               <div class="card-body">
@@ -301,6 +274,6 @@ function createHTML() {
 //           </div>
 //       </div>
 //   </body>
-  
+
 //   </html>`;
 // }
